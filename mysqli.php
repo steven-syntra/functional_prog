2b9b1cf90b3a18a0eee067b6b0dcc9c53f944a7f
Steven De Ryck <steven@inform.be>
2021-11-25 15:03:56 +0100

    Initial commit

---
+++ mysqli.php
@@ -0,0 +1,68 @@
+<h1>MySQLi Example</h1>
+
+<?php
+require_once "config.php";
+//require_once "string_functions.php";
+//require_once "date_functions.php";
+//require_once "business_logic_functions.php";
+//require_once "database.php";
+
+// Create connection
+global $servername, $username, $password, $dbname; //eigenlijk overbodig, enkel voor PHPStorm
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//define and execute query
+$sql = "select * from gemeente inner join provincie on det_prv_id = prv_id";
+$result = $conn->query($sql);
+
+print "<table>";
+
+//show result (if there is any)
+if ( $result->num_rows > 0 )
+{
+    // output data of each row
+    $aantal = 0;
+    while( $row = $result->fetch_assoc() )
+    {
+        $aantal++;
+
+        if ( $aantal > 10 ) break;
+
+        //var_dump($row); print "<br>";
+        echo "<tr>";
+        print "<td>" . $row["det_id"] . "</td>";
+        print "<td>" . $row["det_niscode"] . "</td>";
+        print "<td>" . $row["prv_naam"] . "</td>";
+        print "<td>" . $row["det_txt_nl"] . "</td>";
+        print "<td>" . $row["det_txt_fr"] . "</td>";
+        print "<td>" . $row["det_cases"] . "</td>";
+
+        if ( $row["det_image"] > "" ) {
+            $afbeeldingslink = "<img src=" . $row["det_image"] . " />";
+        }
+        else {
+            $afbeeldingslink = null;
+        }
+
+        print "<td>$afbeeldingslink</td>";
+
+        print "</tr>";
+    }
+}
+else
+{
+    echo "No records found";
+}
+
+print "</table>";
+
+HaliHalo();
+
+$conn->close();
+?>
---
+++ pompom.php
@@ -0,0 +1,15 @@
+<h1>Dit is pompom</h1>
+
+<?php
+print "<p>Dit is PHP code</p>";
+print "<p>Dit is PHP code</p>";
+print "<p>Dit is PHP code</p>";
+?>
+
+<h1>Dit is pompom</h1>
+
+<?php
+print "<p>Dit is PHP code</p>";
+print "<p>Dit is PHP code</p>";
+print "<p>Dit is PHP code</p>";
+?>
