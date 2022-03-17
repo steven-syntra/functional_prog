f2ee444588e25a6dd304be103dbdda8f9fef0cd7
steven-syntra <info@inform.be>
2022-03-17 15:24:07 +0100

    oef11.6

---
+++ oef11.6/lib/config.php
@@ -0,0 +1,7 @@
+<?php
+$logfile = $_SERVER['DOCUMENT_ROOT'] . $app_root . "/log/log.txt";
+$dbconfig = [ "server" => "localhost",
+    "user" => "root",
+    "password" => "",
+    "database" => "steden"
+];
